e04362cfbfc06d274f61e899020c69c089f9fbe7
HarshSharma225 <work.harshsharma.07@gmail.com>
2025-06-07 22:02:03 +0530

    first commit/ authentication

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const url = import.meta.env.VITE_APPWRITE_URL;
+  console.log(url);
+  return(
+  <>
+    <h1> Radhe Radhe</h1>
+    
+  </>
+  )
+  
+}
+
+export default App
